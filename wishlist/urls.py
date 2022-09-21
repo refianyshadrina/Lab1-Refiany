c03f79ab361fb6ca48483ba31951108a990e7697
refianys1678 <refianys1678@gmail.com>
2022-09-21 22:14:45 +0700

    lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from wishlist.views import show_wishlist
 from wishlist.views import show_json
 from wishlist.views import show_json_by_id
+from wishlist.views import register 
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
